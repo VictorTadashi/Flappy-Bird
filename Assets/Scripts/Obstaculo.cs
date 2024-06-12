a27ab14e06dfda436c33e07e84e142cc6ab05248
Tadashi <victortadashisaitobarra@gmail.com>
2024-06-12 13:38:51 -0300

    Aula pontuação

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -7,17 +7,35 @@ public class Obstaculo : MonoBehaviour
     [SerializeField]
     private float velocidade = 0.6f;
 
+    private Vector3 posicaoPassaro;
+
+    private bool pontuei;
+
+    private UIscript scriptDaUI;
+
     [SerializeField]
     private float variacaoY;
     private void Awake()
     {
+        this.transform.Translate(Vector3.up * -3);
         this.transform.Translate(Vector3.up * Random.Range(-variacaoY,variacaoY));
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(!this.pontuei && this.transform.position.x < posicaoPassaro.x)
+        {
+            Debug.Log("Pontoou");
+            this.pontuei = true;
+            this.scriptDaUI.adicionarPontos();
+        }
         this.transform.Translate(Vector3.left * velocidade * Time.deltaTime);
         
+    }
+    private void Start()
+    {
+        this.posicaoPassaro = GameObject.FindObjectOfType<Bird>().transform.position;
+        this.scriptDaUI = GameObject.FindObjectOfType<UIscript>();
     }
 }
